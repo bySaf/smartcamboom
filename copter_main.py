66f849bc57de7a5175673f66449ec6cbecd697e1
David Safarov <81034943+bySaf@users.noreply.github.com>
2023-11-21 09:06:24 +0300

    Add files via upload

---
+++ copter_main.py
@@ -0,0 +1,53 @@
+import rospy
+from clover import srv
+from std_srvs.srv import Trigger
+import math, socket
+
+
+def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='', auto_arm=False, tolerance=0.2):
+    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
+
+    while not rospy.is_shutdown():
+        telem = get_telemetry(frame_id='navigate_target')
+        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
+            break
+        rospy.sleep(0.2)
+
+
+HOST = ("192.168.11.103", 9090)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(HOST)
+print("Connected to", HOST)
+
+msg = client.recv(1024)
+print(msg.decode('UTF-8'))
+
+arr = list(map(int, msg.split()))
+
+x = arr[0]
+y = arr[1]
+
+rospy.init_node('flight')
+
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
+set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
+set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
+set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
+set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
+land = rospy.ServiceProxy('land', Trigger)
+
+navigate_wait(x=0, y=0, z=1, frame_id='body', auto_arm=True)
+rospy.sleep(2)
+
+
+navigate_wait(x=x, y=y, z=1, yaw=float('nan'), frame_id='aruco_map')
+rospy.sleep(2)
+
+navigate_wait(x=0, y=0, z=1, yaw=float('nan'), frame_id='aruco_map')
+rospy.sleep(2)
+
+land()
+
